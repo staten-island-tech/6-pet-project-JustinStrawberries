3ae2566439a044c6c0ff06aac6527cbe2985c5fc
JustinStrawberries <Justinl870@nycstudents.net>
2026-04-17 10:17:19 -0400

    pet and stuff

---
+++ jillian.py
@@ -0,0 +1,31 @@
+class Mage:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+Justin = Mage("Justin", 20, ["Gem"])
+Justin.buy({"title": "Wand", "atk": 2})
+print(Justin.__dict__)
+
+
+# class Hero:
+#     def __init__(self, name, money, inventory):
+#         self.name = name
+#         self.money = money
+#         self.inventory = inventory
+
+#     def buy(self, item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+
+# Jillian = Hero("Jillian", 150, ["Potion"])
+
+# print (Jillian.__dict__)
+
+# Jillian.buy({"title": "Sword", "atk": 34})
+# print(Jillian.__dict__)
---
+++ pet.py
@@ -0,0 +1,21 @@
+class Dog:
+    def __init__(self, name, happiness, toys):
+        self.name = name
+        self.happiness = happiness
+        self.toys = toys
+
+    def play(self, fetch):
+        
+# class Mage:
+#     def __init__(self, name, money, inventory):
+#         self.name = name
+#         self.money = money
+#         self.inventory = inventory
+
+#     def buy(self, item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+
+# Justin = Mage("Justin", 20, ["Gem"])
+# Justin.buy({"title": "Wand", "atk": 2})
+# print(Justin.dict)
