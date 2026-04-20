fcce8db90757e215d022449c21a47eee85a4f1e6
JustinStrawberries <Justinl870@nycstudents.net>
2026-04-20 10:17:00 -0400

    wawadwadaw

--- jillian.py
+++ jillian.py
@@ -1,19 +1,4 @@
-class Mage:
-    def __init__(self, name, money, inventory):
-        self.name = name
-        self.money = money
-        self.inventory = inventory
-
-    def buy(self, item):
-        self.inventory.append(item)
-        print(self.inventory)
-
-Justin = Mage("Justin", 20, ["Gem"])
-Justin.buy({"title": "Wand", "atk": 2})
-print(Justin.__dict__)
-
-
-# class Hero:
+# class Mage:
 #     def __init__(self, name, money, inventory):
 #         self.name = name
 #         self.money = money
@@ -23,9 +8,24 @@ print(Justin.__dict__)
 #         self.inventory.append(item)
 #         print(self.inventory)
 
-# Jillian = Hero("Jillian", 150, ["Potion"])
+# Justin = Mage("Justin", 20, ["Gem"])
+# Justin.buy({"title": "Wand", "atk": 2})
+# print(Justin.__dict__)
 
-# print (Jillian.__dict__)
 
-# Jillian.buy({"title": "Sword", "atk": 34})
-# print(Jillian.__dict__)
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+
+Jillian = Hero("Jillian", 150, ["Potion"])
+
+print (Jillian.__dict__)
+
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__)
--- pet.py
+++ pet.py
@@ -1,11 +1,37 @@
 class Dog:
     def __init__(self, name, happiness, toys):
         self.name = name
-        self.happiness = happiness
+        self.__happiness = happiness
         self.toys = toys
 
-    def play(self, fetch):
-        
+    def play(self, amount):
+        self.__happiness += amount
+
+    def show_happiness(self):
+        print(f"{self.name} has ${self.__happiness}")
+
+Odie = Dog("Odie", 90, ["Bully Stick"])
+
+
+print(Odie.__dict__)
+
+Odie.play(10)
+
+print(Odie.__dict__)
+
+# class BankAccount:
+#     def __init__(self, owner, balance):
+#         self.owner = owner
+#         self.__balance = balance  # double underscore means "private"
+
+#     def deposit(self, amount):
+#         self.__balance += amount
+
+#     def show_balance(self):
+#         print(f"{self.owner} has ${self.__balance}")
+
+
+
 # class Mage:
 #     def __init__(self, name, money, inventory):
 #         self.name = name
@@ -18,4 +44,5 @@ class Dog:
 
 # Justin = Mage("Justin", 20, ["Gem"])
 # Justin.buy({"title": "Wand", "atk": 2})
-# print(Justin.dict)
+# print(Justin.dict) skibidi:
+# 
