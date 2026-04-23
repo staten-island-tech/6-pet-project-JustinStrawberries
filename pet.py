d09b73dbeab59ffb80b32a9ea1bf152fa2799d41
JustinStrawberries <Justinl870@nycstudents.net>
2026-04-23 10:17:05 -0400

    henry mayerjhoff

--- pet.py
+++ pet.py
@@ -1,15 +1,24 @@
 class Dog:
-    def __init__(self, name, happiness, toys):
+    def __init__(self, name, happiness, toys, energy):
         self.name = name
         self.happiness = happiness
         self.toys = toys
+        self.energy = energy
 
     def play(self, minutes):
         happy = minutes*2
-        self.happiness += happy
-        if self.happiness > 100:
-            self.happiness = 100
-        print(f"{self.name}, played fetch!")
+        energy_used = minutes//2
+        if self.energy >= energy_used:
+            self.energy -= energy_used
+            self.happiness += happy
+            if self.happiness > 100:
+                self.happiness = 100
+                print(f"{self.name}, played fetch for {minutes} minutes!")
+            return False
+        else:
+            print(f"Your pet's energy is too low to play for {minutes} minutes")
+            return True
+
 
     def show_happiness(self):
         print(f"{self.name} has ${self.happiness}")
@@ -31,9 +40,9 @@ class Cat:
         print(f"{self.name} has ${self.happiness}")
 
 
-Odie = Dog("Odie", 90, ["Bully Stick"])
-Fluffy = Dog("Fluffy", 20, ["Red Collar"])
-Mr_Meows = Cat("Mr. Meows", 45, {""})
+Odie = Dog("Odie", 90, ["Bully Stick"], 10)
+Fluffy = Dog("Fluffy", 20, ["Red Collar"], 20)
+Mr_Meows = Cat("Mr. Meows", 45, [""])
 Ms_Meows = Cat("Ms. Meows", 70, ["Fish"])
 
 
@@ -42,15 +51,23 @@ pet = Odie
 print(Odie.__dict__)
 
 while True:
-    x = int(input(f"How many minutes do you to spend playing with {pet.name}? '(max 60)'"))
-    if 0 < x <= 60:
-        break
-    else:
-        print('Enter a number from 1-60!')
+    user_input = input(f"How many minutes do you to spend playing with {pet.name}? (max 60)")
+
+    try:
+        x = int(user_input)
+
+        if 0 < x <= 60:
+            break
+        else:
+            print('Enter a number from 1-60!')
+    except ValueError:
+        print('Not a viable input')
     
 
 pet.play(x)
 print(f"Odie has '{Odie.happiness}' happiness now.")
+print(f"Odie has '{Odie.energy}' energy now.")
+
 
 
 # class BankAccount:
